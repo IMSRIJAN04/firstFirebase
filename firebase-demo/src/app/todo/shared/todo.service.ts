61a93a19bbc06ba0c88a5a9c92cc76d375b86d5f
IMSRIJAN04 <srijanerroju@gmail.com>
2020-04-09 00:14:29 +0530

    working on the to do list services

--- firebase-demo/src/app/todo/shared/todo.service.ts
+++ firebase-demo/src/app/todo/shared/todo.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { from } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  toDoList: AngularFireList<any>;
+  constructor(private firebasedb: AngularFireDatabase) { }
 
-  constructor() { }
+  getToDoList() {
+    this.toDoList = this.firebasedb.list('titles');
+    return this.toDoList;
+  }
+
+  
+
 }

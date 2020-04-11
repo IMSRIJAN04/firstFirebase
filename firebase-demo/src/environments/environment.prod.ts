c785d8736d9c5209a0969a2385b5c20271e2a63a
IMSRIJAN04 <srijanerroju@gmail.com>
2020-04-11 15:51:12 +0530

    deployed basic to do app

--- firebase-demo/src/environments/environment.prod.ts
+++ firebase-demo/src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+
+
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAXK3cZNaPXsxL7Kxm7BBAqXFGlN-n6A14",
+    authDomain: "todolist-50218.firebaseapp.com",
+    databaseURL: "https://todolist-50218.firebaseio.com",
+    projectId: "todolist-50218",
+    storageBucket: "todolist-50218.appspot.com",
+    messagingSenderId: "556183276017",
+    appId: "1:556183276017:web:0cb87bb7807c3bca5c5c50",
+    measurementId: "G-ER0P401WGG"
+  }
+    
+ 
 };

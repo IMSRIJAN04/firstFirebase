822bf19e11ba3f9ddd6db6d99d345f05f32c7baa
IMSRIJAN04 <srijanerroju@gmail.com>
2020-04-11 19:44:39 +0530

    routing and router link

--- firebase-demo/src/app/app-routing.module.ts
+++ firebase-demo/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
+import { TodoComponent } from './todo/todo.component';
+import { AboutComponent } from './about/about.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 
 const routes: Routes = [
- 
+  {path:'', component:WelcomeComponent},
+  {path:"todo", component:TodoComponent},
+  {path:"about", component:AboutComponent}
 ];
 
 @NgModule({

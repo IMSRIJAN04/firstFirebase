c2a2e60fb679e96ad03c0c2f0bc95ac1feceb700
IMSRIJAN04 <srijanerroju@gmail.com>
2020-04-13 23:32:35 +0530

    todoappWrappingup

--- firebase-demo/src/app/app.module.ts
+++ firebase-demo/src/app/app.module.ts
@@ -1,33 +1,21 @@
 import{ environment } from './../environments/environment';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-import { from } from 'rxjs';
-import { TodoComponent } from './todo/todo.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { AboutComponent } from './about/about.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoComponent,
-    WelcomeComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
-
     BrowserModule,
-    AppRoutingModule, 
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    MDBBootstrapModule.forRoot()
+    AppRoutingModule,
+    
   ],
-  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
